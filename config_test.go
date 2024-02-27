ad42b21859017d0876e316f647224221b1a0f4a7
isayme <isaymeorg@gmail.com>
2024-02-27 23:16:05 +0800

    update test

--- config_test.go
+++ config_test.go
@@ -24,8 +24,21 @@ func TestParseConfig(t *testing.T) {
 			Addr string `yaml:"addr"`
 		}
 		var cfg Config
-		err := parseConfig([]byte(`{"addr": "example.com"}`), "yaml", &cfg)
+		err := parseConfig([]byte(`addr: "example.com"`), "yaml", &cfg)
 		require.Nil(err)
 		require.Equal(cfg.Addr, "example.com")
+	})
+
+	t.Run("default value", func(t *testing.T) {
+		type Config struct {
+			Addr string `yaml:"addr"`
+		}
+		var cfg Config = Config{
+			Addr: "abc",
+		}
+
+		err := parseConfig([]byte(``), "yaml", &cfg)
+		require.Nil(err)
+		require.Equal(cfg.Addr, "abc")
 	})
 }
